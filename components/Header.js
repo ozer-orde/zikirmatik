65ce13298d88640ef1b70e6652c799bcc2490a17
expholigan <ozer.orde@std.yeditepe.edu.tr>
2024-12-08 22:21:53 +0300

    v1

---
+++ components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import '@/styles/header.css'
+
+function Header() {
+    return (
+        <div>
+            <div className='header'>
+                <div className='baslik'>
+                    <span className='zikirmatik'>Zikirmatik</span>
+                    <span className='tm'>TM</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
